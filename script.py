24124e148282cfb470f6332b27ea29015f69cd33
Elena1505 <eb.lelou@gmail.com>
2023-03-03 11:38:11 +0100

    Start the model (RandomForestClassifier) with mlflow

--- script.py
+++ script.py
@@ -14,6 +14,12 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 import xgboost as xgb
+import logging
+import mlflow.sklearn
+import sys
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
 
 
 # Preprocess application_train.csv
@@ -74,3 +80,29 @@ if __name__ == "__main__":
     grid = GridSearchCV(pipe, param_grid, cv=5, return_train_score=True)
     grid.fit(test_x, test_y)
     print("Best: ", grid.best_score_, "using ", grid.best_params_)
+
+    # Model parameters
+    n_estimators = float(sys.argv[2]) if len(sys.argv) > 1 else 100
+    min_samples_split = float(sys.argv[1]) if len(sys.argv) > 2 else 2
+
+    # Start the model with mlflow
+    with mlflow.start_run():
+        clf = RandomForestClassifier(n_estimators=n_estimators, min_samples_split=min_samples_split)
+        steps_model = [("ohe", OneHotEncoder(handle_unknown="ignore")), ("model", clf)]
+        pipe_model = Pipeline(steps_model)
+        pipe_model.fit(train_x, train_y)
+
+        predicted_qualities = pipe_model.predict(test_x)
+
+        (accuracy, f1_score, roc) = eval_metrics(test_y, predicted_qualities)
+
+        print("Random Forest Classifier model (alpha={:f}, l1_ratio={:f}):".format(n_estimators, min_samples_split))
+        print("  Accuracy: %s" % accuracy)
+        print("  ROC: %s" % roc)
+        print("  F1 score: %s" % f1_score)
+
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_param("min_samples_split", min_samples_split)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("ROC", roc)
+        mlflow.log_metric("f1_score", f1_score)
